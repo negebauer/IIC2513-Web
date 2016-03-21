46d9d967f28b2c467ba8ab848b43669c12f6b2c9
negebauer <negebauer@uc.cl>
2016-03-20 23:52:13 -0300

    Ayudantia

---
+++ E2_camels/camel2.rb
@@ -0,0 +1,40 @@
+class Camel_Race
+
+  attr_reader :camel_quantity, :camel_data, :race_data
+
+  def initialize()
+    @camel_quantity = ""
+    @camel_data = ""
+    @race_data = ""
+  end
+
+  def read_file
+    file = File.open("/Users/Nico/Dropbox/PUC/01 Cursos/2016-1/IIC2513 Tecnologías y Aplicaciones Web/IIC2513Web/E2_camels/carreras_ejemplo_v1.1/carrera_2_camellos.txt", "r")
+
+    count = 0
+    file.each_line do |line|
+      if count == 0
+        @camel_quantity = line
+      elsif count == 1
+        @camel_data = line
+      else
+        @race_data << line
+      end
+      count += 1
+    end
+    file.close
+  end
+
+  def get_data()
+    data = @race_data.scan(/\w+\s*,\s*[0-9\.]+/) # { |match|  }
+    puts data
+  end
+end
+
+a = Camel_Race.new()
+a.read_file
+# puts a.instance_variable_get(:@camel_quantity)
+puts a.camel_quantity
+puts a.camel_data
+# puts a.race_data
+a.get_data
