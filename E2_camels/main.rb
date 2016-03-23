c51453d41f7c973f49d61b03a769fddd363a7e11
negebauer <negebauer@uc.cl>
2016-03-22 21:26:45 -0300

    Actualizados archivos. Recibir archivo carrera como comando u usar carrera_2

--- E2_camels/main.rb
+++ E2_camels/main.rb
@@ -1,8 +1,14 @@
 require_relative 'camel_race'
 
+# Si nos llaman de terminal usamos el archivo que nos pasan ahi
+dir = ARGV[0]
+if dir.nil?
+  # Si corremos desde nuestro IDE ruby preferido (Atom <3)
+  dir = __dir__ + "/carreras ejemplo v1.3/carrera_2_camellos.txt"
+end
+
 a = Camels::Camel_Race.new()
-#a.read_file("/Users/Nico/Dropbox/PUC/01 Cursos/2016-1/IIC2513 Tecnologías y Aplicaciones Web/IIC2513Web/E2_camels/carreras ejemplo v1.2/carrera_2_camellos.txt")
-a.read_file("/home/david/IIC2513Web/E2_camels/carreras ejemplo v1.2/carrera_2_camellos.txt")
+a.read_file(dir)
 # puts a.instance_variable_get(:@camel_quantity)
  puts a.camel_quantity
  puts a.camel_data
