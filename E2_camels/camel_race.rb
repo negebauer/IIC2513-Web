12a72799c6e93e42a58c8821351207af380c7f1a
negebauer <negebauer@uc.cl>
2016-03-25 01:32:15 -0300

    Basura

--- E2_camels/camel_race.rb
+++ E2_camels/camel_race.rb
@@ -1,207 +1,189 @@
 module Camels
-	require_relative 'camel'
+  require_relative 'camel'
 
-	class Camel_Race
+  class Camel_Race
+    @@total_distance = 3210
+    @@lap_distance = @@total_distance / 5
 
-		@@total_distance = 3210
-		@@lap_distance = @@total_distance / 5
+    def self.total_distance
+      @@total_distance
+    end
 
-		def Camel_Race.total_distance
-			return @@total_distance
+    def self.lap_distance
+      @@lap_distance
+    end
+
+    attr_reader :camel_quantity, :camel_data, :race_data, :camels, :data_race, :winners
+
+    def initialize
+      @camel_quantity = ''
+      @camel_data = ''
+      @race_data = ''
+      @camels = {}
+      @data_race = []
+      @positions = {}
+      @winners = {}
+      @winner = []
+      @points = {}
+    end
+
+    def inspect
+      str = "Carrera con #{@camels.count} camellos\n"
+      @camels.keys.each { |key| str += "#{@camels[key].inspect}\n" }
+      str
+    end
+
+    def read_file(dir)
+      file = File.open(dir, 'r')
+      count = 0
+      file.each_line do |line|
+        if count == 0
+          @camel_quantity = line
+        elsif count == 1
+          @camel_data = line
+          create_cammels
+        else
+          @race_data << line
+        end
+        count += 1
+      end
+      file.close
+    end
+
+    def create_cammels
+      camels_with_id = @camel_data.split(';')
+      camels_with_id.each do |camel_id|
+        camel_id_array = camel_id.split(',')
+        return if camel_id_array[0].nil? || camel_id_array[1].nil?
+        camel_name = camel_id_array[0].strip
+        camel_id = camel_id_array[1].strip
+        if @camels[camel_id].nil?
+          camel = Camels::Camel.new(camel_name, camel_id)
+          @camels[camel_id] = camel
+        end
+      end
+    end
+
+		def get_raw_data
+      @data_race = @race_data.scan(/\w+\s*,\s*[0-9\.]+/)
 		end
 
-		def Camel_Race.lap_distance
-			return @@lap_distance
-		end
+    def process_data
+      return if @data_race.empty?
+      @data_race.each do |race|
+        race = race.split(',')
+        return if race.empty?
+        id = race[0].strip
+        distance = race[1].strip.to_f
+        @camels[id].distances.push(distance) unless @camels[id].nil?
+      end
+      @camels.keys.each { |key| camels[key].generate_laps_time }
+    end
 
-		attr_reader :camel_quantity, :camel_data, :race_data, :camels, :data_race, :winners
+    def process_winner
+      camels = @camels.values
+      camels.sort_by!(&:total_time)
+      camels.select! { |camel| camel.total_time == camels[0].total_time }
+      @winner = camels
+    end
 
-	  def initialize
-	    @camel_quantity = ""
-	    @camel_data = ""
-	    @race_data = ""
-			@camels = Hash.new
-			@data_race = []
-			@positions = Hash.new
-			@winners = Hash.new
-			@winner = []
-			@points = Hash.new
-	  end
+    def empty_winners
+      @winners = {}
+    end
 
-		def inspect
-			str = "Carrera con #{@camels.count.to_s} camellos\n"
-			@camels.keys.each { |key| str += "#{@camels[key].inspect}\n" }
-			return str
-		end
+    def empty_winners_lap(lap)
+      @winners[lap] = { 1 => [], 2 => [], 3 => [] }
+    end
 
-	  def read_file(dir)
-	    file = File.open(dir, "r")
-	    count = 0
-	    file.each_line do |line|
-	      if count == 0
-	        @camel_quantity = line
-	      elsif count == 1
-	        @camel_data = line
-					create_cammels
-	      else
-	        @race_data << line
-	      end
-	      count += 1
-	    end
-	    file.close
-	  end
+    def number_of_winners(lap)
+      sum = 0
+      @winners[lap].keys.each { |key| sum += winners[lap][key].count }
+      sum
+    end
 
-		def create_cammels
-			camels_with_id = @camel_data.split(";")
-			camels_with_id.each { |camel_id|
-				camel_id_array = camel_id.split(",")
-				if camel_id_array[0].nil? || camel_id_array[1].nil?
-					return
-				end
-				camel_name = camel_id_array[0].strip
-				camel_id = camel_id_array[1].strip
-				if @camels[camel_id].nil?
-					camel = Camels::Camel.new(camel_name, camel_id)
-					@camels[camel_id] = camel
-				end
-			}
-		end
+    def process_winners
+      camels = @camels.values
+      empty_winners
+      (1..5).each do |lap|
+        empty_winners_lap(lap)
+        position = 1
+        last_camel_time = 0
+        camels.sort_by! { |camel| camel.times[lap] }
+        camels.each do |camel|
+          if last_camel_time == 0 || camel.times[lap] <= last_camel_time
+            last_camel_time = camel.times[lap]
+            @winners[lap][position].push(camel)
+          elsif position < 3
+            next if number_of_winners(lap) >= 3
+            position += 1 if @winners[lap][position].count >= 2
+            position += 1
+            last_camel_time = camel.times[lap]
+            @winners[lap][position].push(camel)
+          else
+            next
+          end
+        end
+      end
+    end
 
-	  def get_raw_data
-	    @data_race = @race_data.scan(/\w+\s*,\s*[0-9\.]+/) # { |match|  }
-		end
+    def process_points
+      camels = @camels.values
+      camels.each { |camel| @points[camel] = 0 }
+      (1..3).each do |lap|
+        @winners[lap].keys.each do |position|
+          camels = @winners[lap][position]
+          points = 0
+          if position == 1
+            points = 6
+          elsif position == 2
+            points = 4
+          elsif position == 3
+            points = 1
+          end
+          camels.each do |camel|
+            @points[camel] += points
+          end
+        end
+      end
+    end
 
-		def process_data
-			if @data_race.empty?
-				return
-			end
-			@data_race.each do |race|
-				race = race.split(",")
-				if race.empty?
-					return
-				end
-				id = race[0].strip
-				distance = race[1].strip.to_f
-				if !@camels[id].nil?
-					@camels[id].distances.push(distance)
-				end
-			end
-			@camels.keys.each { |key| camels[key].generate_laps_time }
-		end
+    def camels_str
+      str = "Camellos:\n"
+      @camels.values.each { |camel| str += "#{camel.name}, #{camel.id}\n" }
+      str += "\n"
+      str
+    end
 
-		def process_winner
-			camels = @camels.values
-			camels.sort_by! { |camel| camel.total_time }
-			camels.select! { |camel| camel.total_time == camels[0].total_time }
-			@winner = camels
-		end
+    def laps_str
+      str = "Resultado vueltas:\n"
+      @winners.keys.each do |lap|
+        str += "Vuelta #{lap}\n"
+        @winners[lap].keys.each do |position|
+          next if @winners[lap][position][0].nil?
+          str += position.to_s
+          @winners[lap][position].each do |camel|
+            str += ',' if camel != @winners[lap][position][0]
+            str += " #{camel.name}"
+          end
+          str += " (#{@winners[lap][position][0].times[lap]} seg)\n"
+        end
+        str += "\n"
+      end
+      str
+    end
 
-		def empty_winners
-			@winners = Hash.new
-		end
+    def winner_str
+      if @winner.count == 1
+        return "Ganador del Derby del Gran Camejockey: #{@winner[0].name}\n\n"
+      else
+        return "Ganadores del Derby del Gran Camejockey:\n#{@winner.map { |camel| "#{camel.name}\n" }}\n\n"
+      end
+    end
 
-		def empty_winners_lap(lap)
-			@winners[lap] = {1 => [], 2 => [], 3 => []}
-		end
-
-		def number_of_winners(lap)
-			sum = 0
-			@winners[lap].keys.each { |key| sum += winners[lap][key].count }
-			return sum
-		end
-
-		def process_winners
-			camels = @camels.values
-			empty_winners
-			(1..5).each { |lap|
-				empty_winners_lap(lap)
-				position = 1
-				last_camel_time = 0
-				camels.sort_by! { |camel| camel.times[lap] }
-				camels.each { |camel|
-					if last_camel_time == 0 || camel.times[lap] <= last_camel_time
-						last_camel_time = camel.times[lap]
-						@winners[lap][position].push(camel)
-					elsif position < 3
-						if number_of_winners(lap) >= 3
-							next
-						end
-						if @winners[lap][position].count >= 2
-							position += 1
-						end
-						position += 1
-						last_camel_time = camel.times[lap]
-						@winners[lap][position].push(camel)
-					else
-						next
-					end
-				}
-			}
-		end
-
-		def process_points
-			camels = @camels.values
-			camels.each { |camel| @points[camel] = 0 }
-			(1..3).each { |lap|
-				@winners[lap].keys.each { |position|
-					camels = @winners[lap][position]
-					points = 0
-					if position == 1
-						points = 6
-					elsif position == 2
-						points = 4
-					elsif position == 3
-						points = 1
-					end
-					camels.each { |camel|
-						@points[camel] += points
-					}
-				}
-			}
-		end
-
-		def camels_str
-			str = "Camellos:\n"
-			@camels.values.each { |camel| str += "#{camel.name}, #{camel.id}\n" }
-			str += "\n"
-			return str
-		end
-
-		def laps_str
-			str = "Resultado vueltas:\n"
-			@winners.keys.each { |lap|
-				str += "Vuelta #{lap}\n"
-				@winners[lap].keys.each { |position|
-					if @winners[lap][position][0].nil?
-						next
-					end
-					str += "#{position}"
-					@winners[lap][position].each { |camel|
-						if camel != @winners[lap][position][0]
-							str += ","
-						end
-						str += " #{camel.name}"
-					}
-					str += " (#{@winners[lap][position][0].times[lap]} seg)\n"
-				}
-				str += "\n"
-			}
-			return str
-		end
-
-		def winner_str
-			if @winner.count == 1
-				return "Ganador del Derby del Gran Camejockey: #{@winner[0].name}\n\n"
-			else
-				return "Ganadores del Derby del Gran Camejockey:\n#{@winner.map { |camel| "#{camel.name}\n" }}\n\n"
-			end
-		end
-
-		def points_str
-			str = "Puntajes:\n"
-			@points.keys.each { |camel| str += "#{camel.name}: #{@points[camel]}\n" }
-			return str
-		end
-
-	end
+    def points_str
+      str = "Puntajes:\n"
+      @points.keys.each { |camel| str += "#{camel.name}: #{@points[camel]}\n" }
+      str
+    end
+  end
 end
