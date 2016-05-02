2a6a62e97fa856713d8a60fb17ea28cea7415f2e
negebauer <negebauer@uc.cl>
2016-05-02 00:28:06 -0300

    Agregado mail y active a usuario

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160429201126) do
+ActiveRecord::Schema.define(version: 20160502032725) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username",   null: false
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20160429201126) do
     t.boolean  "admin"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "email"
+    t.boolean  "active"
   end
 
   add_index "users", ["username"], name: "index_users_on_username", unique: true
