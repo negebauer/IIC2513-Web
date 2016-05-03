4b00f71d58a31fe9bd59840ff8e7ea1c145fa133
daruz14 <daruz@uc.cl>
2016-05-03 15:11:41 -0300

    Se arregla el validate del user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,16 @@ class User < ActiveRecord::Base
 
   validates :username,  presence: true
   validates :password,  presence: true
-  validates :email, presence: true
+  validate :check_email_format
 
+  def check_email_format
+    if self.email == ""
+        errors.add("email","Debe ingresar un email")
+    else
+      errors.add("email","Ingrese uno con el formato correcto") unless
+      self.email && self.email.match("^[^@]+@[^@]+\.[^@]+$")
+    end
+  end
 
 
 end
