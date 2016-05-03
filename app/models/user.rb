6c43e065a1b9f718f0e357be23058b75547fe7e4
daruz14 <daruz@uc.cl>
2016-05-03 15:15:15 -0300

    Se borro una funcion fea del validate

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,10 @@ class User < ActiveRecord::Base
 
   validates :username,  presence: true
   validates :password,  presence: true
-  validate :check_email_format
-
-  def check_email_format
-    if self.email == ""
-        errors.add("email","Debe ingresar un email")
-    else
-      errors.add("email","Ingrese uno con el formato correcto") unless
-      self.email && self.email.match("^[^@]+@[^@]+\.[^@]+$")
-    end
-  end
+  #validate :check_email_format
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
+                  format: { with: VALID_EMAIL_REGEX }
 
 
 end
