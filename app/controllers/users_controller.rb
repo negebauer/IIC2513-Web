2d6129ebdc7f13f4b657bac06fb65b9cffcfcf5c
negebauer <negebauer@uc.cl>
2016-05-03 22:05:18 -0300

    Index usuario solo visible por admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :user_admin_required, only: [:show, :edit, :update, :destroy]
+  before_action :user_admin_required, only: [:index, :show, :edit, :update, :destroy]
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   # GET /users
