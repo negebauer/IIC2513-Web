966929da40953ea16bd45bff4b108e06ef261e47
negebauer <negebauer@uc.cl>
2016-05-17 19:44:24 -0300

    imagen?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,6 @@
 Rails.application.configure do
     # Para mostrar imagenes
     config.serve_static_assets = true
-    config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
     config.assets.compile = true
 
     # Settings specified here will take precedence over those in config/application.rb.
