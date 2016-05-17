57a7e964bb89bfc6100f3d4dc79ccf4c8a6dc2e0
negebauer <negebauer@uc.cl>
2016-05-17 01:22:21 -0300

    cambios locale. por defecto usuario nuevo activo

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
   validates :username,  presence: true
   validates :password,  presence: true, :on => :create
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true,
-                  format: { with: VALID_EMAIL_REGEX }
-
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
 
   def self.login(username, password)
     user = User.find_by_username(username).try(:authenticate, password)
