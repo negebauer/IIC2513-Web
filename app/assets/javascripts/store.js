6207676b59a36a36d7047c60ba36e032fa140f7e
negebauer <negebauer@uc.cl>
2016-05-29 21:44:52 -0300

    Mejorado ocultacion y como mostrar barra cyccling

--- app/assets/javascripts/store.js
+++ app/assets/javascripts/store.js
@@ -1,22 +1,24 @@
 $(function() {
-  var $cells = $(".celda_p")
-  $("button[name=Back]").on('click', toogle_hidden_cells_back)
-  $("button[name=Next]").on('click', toogle_hidden_cells_next)
+  var $cells = $(".celda_p");
+  $("button[name=Previous]").on('click', toogle_hidden_cells_back);
+  $("button[name=Next]").on('click', toogle_hidden_cells_next);
   // console.log($cells);
-  console.log($("button[name=Back]"));
-  console.log($("button[name=Next]"));
 })
 
 function toogle_hidden_cells_back(ev) {
-  ev.preventDefault()
-  toogle_hidden_cells(false)
+  ev.preventDefault();
+  toogle_hidden_cells(false);
 }
 
 function toogle_hidden_cells_next(ev) {
-  ev.preventDefault()
-  toogle_hidden_cells(true)
+  ev.preventDefault();
+  toogle_hidden_cells(true);
 }
 
 function toogle_hidden_cells(next) {
-  console.log("Next: " + next ? "true" : "false");
+  if next {
+    console.log('next');
+  } else {
+    console.log('back');
+  }
 }
