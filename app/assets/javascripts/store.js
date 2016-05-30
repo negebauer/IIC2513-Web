b82f8733e5661c925fc59e9cc270af7869577262
negebauer <negebauer@uc.cl>
2016-05-29 21:33:25 -0300

    nav bar para ir por paginas hacia el 'lado'

--- app/assets/javascripts/store.js
+++ app/assets/javascripts/store.js
@@ -1,7 +1,23 @@
 $(function() {
   var $cells = $(".celda_p")
-  console.log($cells);
+  $("button[name=Back]").on('click', toogle_hidden_cells_back)
+  $("button[name=Next]").on('click', toogle_hidden_cells_next)
+  // console.log($cells);
 })
+
+function toogle_hidden_cells_back(ev) {
+  ev.preventDefault()
+  toogle_hidden_cells(false)
+}
+
+function toogle_hidden_cells_next(ev) {
+  ev.preventDefault()
+  toogle_hidden_cells(true)
+}
+
+function toogle_hidden_cells(next) {
+  console.log("Next: " + next ? "True" : "False");
+}
 
 // $(function() {
 //   var $buttonSend = $('.promo');
