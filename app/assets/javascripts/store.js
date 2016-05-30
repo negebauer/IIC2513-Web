b0b378004e3730db9f9bcd0bd2921f4846139a51
negebauer <negebauer@uc.cl>
2016-05-29 21:49:37 -0300

    ahhh arreglada tontera

--- app/assets/javascripts/store.js
+++ app/assets/javascripts/store.js
@@ -1,12 +1,13 @@
 $(function() {
   var $cells = $(".celda_p");
-  $("button[name=Previous]").on('click', toogle_hidden_cells_back);
+  $("button[name=Previous]").on('click', toogle_hidden_cells_previous);
   $("button[name=Next]").on('click', toogle_hidden_cells_next);
   // console.log($cells);
 })
 
-function toogle_hidden_cells_back(ev) {
+function toogle_hidden_cells_previous(ev) {
   ev.preventDefault();
+  console.log('toggle previous');
   toogle_hidden_cells(false);
 }
 
@@ -16,9 +17,9 @@ function toogle_hidden_cells_next(ev) {
 }
 
 function toogle_hidden_cells(next) {
-  if (next) {
+  if (next == true) {
     console.log('next');
   } else {
-    console.log('back');
+    console.log('previous');
   }
 }
