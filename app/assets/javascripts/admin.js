76a2f5a0e9afbd42d714023dd5092ea92f08ec60
negebauer <negebauer@uc.cl>
2016-05-30 01:07:46 -0300

    ya tenog mensajes de error

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,3 +1,31 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+$(function() {
+  $('form').each(function(index) {
+    var $form = $(this);
+    $form.on('submit', function(ev) {
+      validate_form(ev, $form)
+    })
+  })
+})
+
+function validate_form(ev, $form) {
+  // NOTE: DEBUG ONLY!!!
+  ev.preventDefault()
+
+  // Validate required
+  var $required = $form.find('.required').each(function(index) {
+    var label = $(this).find('label').text();
+    var value = $(this).find('input').val();
+    if (value == "") {
+      var error_message = "Tienes que llenar el campo: " + label
+      
+    }
+  })
+}
+
+function validate_email(email) {
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
+
+
+// t("activerecord.attributes.user.password")
