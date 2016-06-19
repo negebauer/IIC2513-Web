456387130057b85bf3a43fbd809852472fc00354
negebauer <negebauer@uc.cl>
2016-06-19 13:57:42 -0300

    Agregado destroy a las rutas (@daruz14 77). Arreglado error que impedia que usuario normal vea lista de productos

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::ProductsController < Api::V1::BaseController
-    before_action :admin_required, ony: [:update, :destroy]
+    before_action :admin_required, only: [:update, :destroy]
     before_action :set_uuid, only: [:update, :destroy]
 
     def index
@@ -7,15 +7,12 @@ class Api::V1::ProductsController < Api::V1::BaseController
     end
 
     def update
-        puts '--------- Params ---------'
-        puts params
-
         if product = Product.where(uuid: @uuid).first
             # We have a product with this uuid
             if product.update(product_params)
                 render json: product.to_json, status: 201
             else
-                render json: { message: 'failure updating', params: product_params }, status: 500
+                render json: { message: 'Falló actualización', params: product_params }, status: 500
             end
         else
             # Let's create a new product
@@ -23,7 +20,7 @@ class Api::V1::ProductsController < Api::V1::BaseController
             if product.save
                 render json: product.to_json, status: 201
             else
-                render json: { message: 'failure creating', product: product, params: product_params }, status: 500
+                render json: { message: 'Falló creacion', product: product, params: product_params }, status: 500
             end
         end
     end
@@ -46,8 +43,6 @@ class Api::V1::ProductsController < Api::V1::BaseController
         params.delete(:id)
         params.delete(:action)
     end
-
-
 
     def product_params
         params.permit(:name, :price, :stock, :description, :family, :promotion, :image, :uuid)
--- config/routes.rb
+++ config/routes.rb
@@ -36,7 +36,7 @@ Rails.application.routes.draw do
 namespace :api do
     namespace :v1 do
         get '/', to: 'base#index'
-        resources :products, only: [:index, :update]
+        resources :products, only: [:index, :update, :destroy]
     end
 end
 
