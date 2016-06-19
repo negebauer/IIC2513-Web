116f5500efb4c98313440550e0df04afceb4b94d
negebauer <negebauer@uc.cl>
2016-06-19 14:08:00 -0300

    Actualización no se hace si producto no existe. CLoses #45

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -10,12 +10,15 @@ class Api::V1::ProductsController < Api::V1::BaseController
         if product = Product.where(uuid: @uuid).first
             # We have a product with this uuid
             if product.update(product_params)
-                render json: product.to_json, status: 201
+                render json: product.to_json, status: 200
             else
                 render json: { message: 'Falló actualización', params: product_params }, status: 500
             end
         else
-            # Let's create a new product
+            # Let's create a new product only if PUT
+            unless request.put?
+                return render json: { message: 'Producto no existe' }, status: 404
+            end
             product = Product.new(product_params)
             if product.save
                 render json: product.to_json, status: 201
