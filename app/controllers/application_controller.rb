bb6f8aad29168b516fd5d2d7f60c695faa3ab0eb
negebauer <negebauer@uc.cl>
2016-06-30 09:38:57 -0400

    Botar con bloqueo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,6 +18,7 @@ class ApplicationController < ActionController::Base
   def user_session_required
     user_session_set
     if !@current_user || !@current_user.active
+      @current_user = nil
       redirect_to login_path
       return
     end
