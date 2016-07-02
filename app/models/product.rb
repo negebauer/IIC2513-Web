755d56df7fe6ea95a14c11a043b406049a75988f
negebauer <negebauer@uc.cl>
2016-07-01 21:22:33 -0400

    ocultar parametros que no se necestian

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,7 +8,7 @@ class Product < ActiveRecord::Base
     validates :uuid, uniqueness: true
 
     def as_json(options={})
-        result = super(options.merge(except: [:uuid]))
+        result = super(options.merge(except: [:uuid, :stock, :family, :promotion]))
         result[:id] = "http://nidastore.herokuapp.com/api/v1/products/"
         if !uuid.nil?
             result[:id] += uuid
